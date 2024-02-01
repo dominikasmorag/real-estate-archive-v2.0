e78af783ba3c19fa49298563f70a03e3c61b6fba
= <dominikasmorag@gmail.com>
2024-02-01 23:30:43 +0100

    first commit

---
+++ pojo/Result.java
@@ -0,0 +1,91 @@
+package pl.dominikasmorag.pojo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Result {
+    private int id;
+    private String location;
+    private String description;
+    private float squareFootage;
+    private BigDecimal price;
+    private String link;
+    private Date postingDate;
+    private long duration;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getSquareFootage() {
+        return squareFootage;
+    }
+
+    public void setSquareFootage(float squareFootage) {
+        this.squareFootage = squareFootage;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public Date getPostingDate() {
+        return postingDate;
+    }
+
+    public void setPostingDate(Date postingDate) {
+        this.postingDate = postingDate;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "id=" + id +
+                ", location='" + location + '\'' +
+                ", description='" + description + '\'' +
+                ", squareFootage=" + squareFootage +
+                ", price=" + price +
+                ", link='" + link + '\'' +
+                ", postingDate=" + postingDate +
+                ", duration=" + duration +
+                '}';
+    }
+}
---
+++ website/ResultInfo.java
@@ -0,0 +1,79 @@
+package pl.dominikasmorag.website;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import pl.dominikasmorag.pojo.Result;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ResultInfo {
+    static List<String> urls;
+    public Elements elements;
+    private Document document;
+
+    public ResultInfo(WebsiteInfo websiteInfo) throws IOException {
+        urls = websiteInfo.urlWithPages();
+        List<Result> results = new ArrayList<>();
+        for(String s : urls) {
+            document = Jsoup.connect(s).timeout(10 * 1000).get();
+            elements = findElements();
+            for(Element element : elements) {
+                String location = findLocation(element);
+                String description = findDescription(element);
+                float squareFootage = findSquareFootage(element);
+                BigDecimal price = findPrice(element);
+                String link = findLink(element);
+                Result result = new Result();
+                result.setLocation(location);
+                result.setDescription(description);
+                result.setSquareFootage(squareFootage);
+                result.setPrice(price);
+                result.setLink(link);
+                results.add(result);
+            }
+        }
+        for(Result r : results) {
+            System.out.println(r);
+        }
+    }
+
+    public Elements findElements() {
+        return document.getElementsByAttributeValueContaining("class", "propertyBox");
+    }
+
+    private String findLocation(Element element) {
+        return element.getElementsByAttributeValue("class", "property_link").text();
+    }
+
+    private String findDescription(Element element) {
+        return element.getElementsByAttributeValue("class", "description").text();
+    }
+
+    private float findSquareFootage(Element element) {
+        return Float.parseFloat(element.getElementsByAttributeValue("class", "area").text().replaceAll("[^0-9,]", "").replace(",", "."));
+    }
+
+    private BigDecimal findPrice(Element element) {
+        String price = element.getElementsByAttributeValue("class", "price").text();
+        if("inf. u dewelopera".equalsIgnoreCase(price)) {
+            return null;
+        }
+        else {
+            return new BigDecimal(price.replaceAll("[^0-9,]", "").replace(",", "."));
+        }
+    }
+
+    private String findLink(Element element) {
+        return element.getElementsByAttributeValue("class", "property_link").attr("href");
+    }
+
+    private Date findPostingDate() {
+        return new Date();
+    }
+}
