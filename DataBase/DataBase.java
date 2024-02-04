a6f289df21946491944974c0aae291dacd063938
= <dominikasmorag@gmail.com>
2024-02-04 13:44:58 +0100

    method to create table

--- DataBase/DataBase.java
+++ DataBase/DataBase.java
@@ -1,4 +1,33 @@
 package pl.dominikasmorag.DataBase;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataBase {
+    private static final String CREATE_RESULT_TABLE =  "CREATE TABLE RESULTS (" +
+            "ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+            "LOCATION VARCHAR(200)," +
+            "DESCRIPTION VARCHAR(300)," +
+            "SQM FLOAT," +
+            "PRICE DECIMAL," +
+            "LINK VARCHAR(400) UNIQUE," +
+            "IMG_URL VARCHAR(300)," +
+            "POSTING_DATE DATE," +
+            "DURATION_IN_MILLIS LONG);" ;
+    private Statement statement;
+    private final Connection connection;
+
+    public DataBase(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void createSchema() throws SQLException {
+        createTable();
+    }
+
+    private void createTable() throws SQLException  {
+        this.statement = connection.createStatement();
+        statement.executeUpdate(CREATE_RESULT_TABLE);
+    }
 }
