b0f0f7aee6e3a4ff552d7adce4b0e2005fe69c39
= <dominikasmorag@gmail.com>
2024-02-06 22:54:38 +0100

    turned this class into a singleton

--- src/main/java/pl/dominikasmorag/website/WebsiteInfo.java
+++ src/main/java/pl/dominikasmorag/website/WebsiteInfo.java
@@ -8,13 +8,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WebsiteInfo {
+    private static WebsiteInfo instance;
     private static final int FIRST_PAGE = 1;
     int numberOfPage;
     String URL1 = "https://domy.pl/mieszkania-sprzedaz-katowice-pl?page=";
     String URL2 = "&ps[advanced_search]=1&ps[sort_order]=rank&ps[sort_asc]=0&ps[location][type]=1&ps[location][text_queue][0]=Katowice&ps[location][text_tmp_queue][0]=Katowice&ps[transaction]=1&ps[type]=1&ps[price_from]=200000&ps[price_to]=500000&limit=75";
 
+    private WebsiteInfo() {}
 
-    public WebsiteInfo() throws IOException {
+    public static WebsiteInfo getInstance() {
+        if(instance == null) {
+            instance = new WebsiteInfo();
+        }
+        return instance;
     }
 
     /** calculates the number of pages **/
