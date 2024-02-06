9a09577e3e004ce03b506e4d8a3b19261ad789b7
= <dominikasmorag@gmail.com>
2024-02-06 22:51:56 +0100

    insert statement in save method

--- src/main/java/pl/dominikasmorag/DataBase/ResultDao.java
+++ src/main/java/pl/dominikasmorag/DataBase/ResultDao.java
@@ -3,22 +3,14 @@ package pl.dominikasmorag.DataBase;
 import pl.dominikasmorag.pojo.Result;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class ResultDao implements DAO<Result> {
-    /*
-    "LOCATION VARCHAR(200)," +
-            "DESCRIPTION VARCHAR(300)," +
-            "SQM FLOAT," +
-            "PRICE DECIMAL," +
-            "LINK VARCHAR(400) UNIQUE," +
-            "IMG_URL VARCHAR(300)," +
-            "POSTING_DATE DATE," +
-            "DURATION_IN_MILLIS LONG);" ;
-     */
+
     private Connection connection;
 
     public ResultDao(Connection connection) {
@@ -38,6 +30,14 @@ public class ResultDao implements DAO<Result> {
     public void save(Result result) throws SQLException {
         PreparedStatement statement = connection.prepareStatement("INSERT INTO " + DataBase.RESULTS_TABLE_NAME +
                 "(LOCATION, SQM, PRICE, LINK, IMG_URL, POSTING_DATE, DURATION) VALUES (?, ?, ?, ?, ?, ?, ?");
+        statement.setString(1, result.getLocation());
+        statement.setFloat(2, result.getSquareFootage());
+        statement.setBigDecimal(3, result.getPrice());
+        statement.setString(4, result.getLink());
+        statement.setString(5, result.getImgUrl());
+        statement.setDate(6, (Date) result.getPostingDate());
+        statement.setLong(7, result.getDuration());
+        statement.executeUpdate();
     }
 
     @Override
