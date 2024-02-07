58acfeaba1e2ca0b8e16064148d0fd418804a617
= <dominikasmorag@gmail.com>
2024-02-07 22:35:46 +0100

    adding all results to db

--- src/main/java/pl/dominikasmorag/Main.java
+++ src/main/java/pl/dominikasmorag/Main.java
@@ -1,15 +1,22 @@
 package pl.dominikasmorag;
 
+import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
 import org.h2.jdbcx.JdbcDataSource;
+import pl.dominikasmorag.DataBase.DAO;
 import pl.dominikasmorag.DataBase.DataBase;
+import pl.dominikasmorag.DataBase.ResultDao;
+import pl.dominikasmorag.pojo.Result;
+import pl.dominikasmorag.website.ResultInfo;
+import pl.dominikasmorag.website.WebsiteInfo;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-        /** args[0] - url, args[1] username, args[2] - password */
 
         JdbcDataSource dataSource = new JdbcDataSource();
         dataSource.setURL("jdbc:h2:C:/Users/domin/git/real-estate-archive-v2.0");
@@ -20,19 +27,21 @@ public class Main {
             Connection connection = dataSource.getConnection();
             DataBase db = new DataBase(connection);
             db.createSchema();
-            System.out.println(connection.isClosed());
-        } catch (SQLException ex) {
+            WebsiteInfo websiteInfo = WebsiteInfo.getInstance();
+            ResultInfo resultInfo = new ResultInfo(websiteInfo);
+            List<Result> results = resultInfo.scrapAllResults();
+            DAO<Result> resultDAO = new ResultDao(connection);
+
+            for(Result r : results) {
+                try {
+                    resultDAO.save(r);
+                } catch (JdbcSQLIntegrityConstraintViolationException ex) {
+                    System.err.println(ex.getMessage());
+                }
+            }
+        } catch (SQLException | IOException ex) {
             ex.printStackTrace();
         }
-
-
-//        try {
-//            WebsiteInfo websiteInfo = new WebsiteInfo();
-//            ResultInfo resultInfo = new ResultInfo(websiteInfo);
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-
     }
 
 }
