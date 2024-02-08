d66ef4ce6bf57a4a64b6f764f1ac20eb2702f971
= <dominikasmorag@gmail.com>
2024-02-08 07:28:44 +0100

    replaced 'get' words with 'find', also created findAllLinks method which return list of string links

--- src/main/java/pl/dominikasmorag/DataBase/ResultDao.java
+++ src/main/java/pl/dominikasmorag/DataBase/ResultDao.java
@@ -3,6 +3,7 @@ package pl.dominikasmorag.DataBase;
 import pl.dominikasmorag.pojo.Result;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,13 +15,37 @@ public class ResultDao implements DAO<Result> {
         this.connection = connection;
     }
     @Override
-    public Optional<Result> get(int id) throws SQLException {
+    public Optional<Result> findById(int id) throws SQLException {
         return Optional.empty();
     }
 
     @Override
-    public List<Result> getAll() throws SQLException {
-        return null;
+    public List<Result> findAll() throws SQLException {
+        List<Result> results = new ArrayList<>();
+        PreparedStatement statement = connection.prepareStatement("SELECT id, " +
+                "location, " +
+                "description, " +
+                "sqm, " +
+                "price, " +
+                "link, " +
+                "img_url, " +
+                "posting_date, " +
+                "web_scraping_timestamp " +
+                "FROM " + DataBase.RESULTS_TABLE_NAME + ";");
+        ResultSet rs = statement.executeQuery();
+        while(rs.next()) {
+            results.add(new Result(
+            rs.getInt(1),
+            rs.getString(2),
+            rs.getString(3),
+            rs.getFloat(4),
+            rs.getBigDecimal(5),
+            rs.getString(6),
+            rs.getString(7),
+            rs.getDate(8),
+            rs.getTimestamp(9)));
+        }
+    return results;
     }
 
     @Override
@@ -42,5 +67,15 @@ public class ResultDao implements DAO<Result> {
     @Override
     public void delete(Result result) throws SQLException {
 
+    }
+
+    public List<String> findAllLinks() throws SQLException {
+        List<String> linkList = new ArrayList<>();
+        PreparedStatement statement = connection.prepareStatement("SELECT link FROM " + DataBase.RESULTS_TABLE_NAME + ";");
+        ResultSet rs = statement.executeQuery();
+        while(rs.next()) {
+            linkList.add(rs.getString(1));
+        }
+        return linkList;
     }
 }
