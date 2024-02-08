5535f68ebe3c28c7eff39694d02dc3e765bba302
= <dominikasmorag@gmail.com>
2024-02-08 07:25:58 +0100

    implemented methods

--- src/main/java/pl/dominikasmorag/user_utilities/export/ExportHTML.java
+++ src/main/java/pl/dominikasmorag/user_utilities/export/ExportHTML.java
@@ -1,2 +1,29 @@
-package pl.dominikasmorag.user_utilities.export;public class ExportHTML {
+package pl.dominikasmorag.user_utilities.export;
+
+import pl.dominikasmorag.DataBase.DAO;
+import pl.dominikasmorag.pojo.Result;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ExportHTML implements ExportStrategy {
+    public List<Result> resultList;
+    private DAO<Result> dao;
+
+    public ExportHTML(DAO<Result> dao) {
+        this.dao = dao;
+    }
+    @Override
+    public void collectData() {
+        try {
+            this.resultList = dao.findAll();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void export() {
+
+    }
 }
