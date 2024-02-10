d1e2d381b84bb2ef36e8d75434ac8f458e7b6919
= <dominikasmorag@gmail.com>
2024-02-10 15:22:36 +0100

    command factory test

--- src/test/java/pl/dominikasmorag/user_utilities/command/CommandFactoryTest.java
+++ src/test/java/pl/dominikasmorag/user_utilities/command/CommandFactoryTest.java
@@ -1,4 +1,44 @@
+package pl.dominikasmorag.user_utilities.command;
+
+import org.junit.jupiter.api.Test;
+import pl.dominikasmorag.DataBase.ResultDao;
+import pl.dominikasmorag.user_utilities.export.ExportCSV;
+import pl.dominikasmorag.user_utilities.export.ExportHTML;
+import pl.dominikasmorag.user_utilities.export.ExportJSON;
+
+import java.sql.Connection;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CommandFactoryTest {
-  
+
+    Connection conn = null;
+    ResultDao resultDao = new ResultDao(conn);
+
+
+    @Test
+    void generateReportHtmlGeneratesHtml() {
+        ExportCommand exportCommandHTML = (ExportCommand)  CommandFactory.createCommand("generate-report html", resultDao);
+        assertInstanceOf(ExportHTML.class, exportCommandHTML.getExportStrategy());
+    }
+
+    @Test
+    void generateReportCsvGeneratesCsv() {
+        ExportCommand exportCommandCSV = (ExportCommand)  CommandFactory.createCommand("generate-report csv", resultDao);
+        assertInstanceOf(ExportCSV.class, exportCommandCSV.getExportStrategy());
+    }
+
+    @Test
+    void generateReportJsonGeneratesJson() {
+        ExportCommand exportCommandJSON = (ExportCommand)  CommandFactory.createCommand("generate-report json", resultDao);
+        assertInstanceOf(ExportJSON.class, exportCommandJSON.getExportStrategy());
+    }
+
+    @Test
+    void anythingElseGeneratesNull() {
+        ExportCommand exportCommand = (ExportCommand) CommandFactory.createCommand("generate-report jso", resultDao);
+        assertNull(exportCommand.getExportStrategy());
+    }
+
+
 }
