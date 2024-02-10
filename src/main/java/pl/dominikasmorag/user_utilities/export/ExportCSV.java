04d443e85c2c7d8d143ae4fd3e2610be74e6f875
= <dominikasmorag@gmail.com>
2024-02-10 15:18:24 +0100

    export as csv

--- src/main/java/pl/dominikasmorag/user_utilities/export/ExportCSV.java
+++ src/main/java/pl/dominikasmorag/user_utilities/export/ExportCSV.java
@@ -1,2 +1,40 @@
-package pl.dominikasmorag.user_utilities.export;public class ExportCSV {
+package pl.dominikasmorag.user_utilities.export;
+
+import pl.dominikasmorag.DataBase.DAO;
+import pl.dominikasmorag.pojo.Result;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+
+public class ExportCSV extends ExportStrategy {
+
+    private final static String EXTENSION = ".csv";
+
+    public ExportCSV(DAO<Result> resultDao) {
+        super(resultDao);
+        fileName += EXTENSION;
+    }
+
+    @Override
+    public void export() {
+        try {
+            collectData();
+            File file =  createFile();
+            BufferedWriter bf = createBufferedWriter(file);
+            saveDataToFile(bf);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    protected void saveDataToFile(BufferedWriter bufferedWriter) {
+        try {
+            bufferedWriter.write("test csv");
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
