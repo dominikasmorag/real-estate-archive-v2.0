c9f465a281b369e955c480e0ed12cd37965e975b
= <dominikasmorag@gmail.com>
2024-02-10 15:18:50 +0100

    exoprt as json

--- src/main/java/pl/dominikasmorag/user_utilities/export/ExportJSON.java
+++ src/main/java/pl/dominikasmorag/user_utilities/export/ExportJSON.java
@@ -1,2 +1,27 @@
-package pl.dominikasmorag.user_utilities.export;public class ExportJSON {
+package pl.dominikasmorag.user_utilities.export;
+
+import pl.dominikasmorag.DataBase.DAO;
+import pl.dominikasmorag.pojo.Result;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class ExportJSON extends ExportStrategy {
+
+    public ExportJSON(DAO<Result> resultDao) {
+        super(resultDao);
+    }
+
+    @Override
+    public void export() {
+    }
+    protected void saveDataToFile(BufferedWriter bufferedWriter) {
+        try {
+            bufferedWriter.write("test json");
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
