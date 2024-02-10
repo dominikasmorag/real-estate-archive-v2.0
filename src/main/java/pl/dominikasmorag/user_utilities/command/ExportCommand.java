c28b9bcca8fa6b5cdf2fa0d8f2f3af35204edca8
= <dominikasmorag@gmail.com>
2024-02-10 15:15:52 +0100

    export command

--- src/main/java/pl/dominikasmorag/user_utilities/command/ExportCommand.java
+++ src/main/java/pl/dominikasmorag/user_utilities/command/ExportCommand.java
@@ -1,8 +1,32 @@
 package pl.dominikasmorag.user_utilities.command;
 
+import pl.dominikasmorag.user_utilities.export.ExportStrategy;
+
+import java.io.IOException;
+
 public class ExportCommand implements Command {
+    private final ExportStrategy exportStrategy;
+
+    public ExportCommand(ExportStrategy exportStrategy) {
+        this.exportStrategy = exportStrategy;
+    }
     @Override
     public void execute() {
+        try {
+            exportStrategy.export();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    public ExportStrategy getExportStrategy() {
+        return exportStrategy;
+    }
+
+    @Override
+    public String toString() {
+        return "ExportCommand{" +
+                "exportStrategy=" + exportStrategy +
+                '}';
     }
 }
