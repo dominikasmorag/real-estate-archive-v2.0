b8bf2dd66a678e25079d04737ba5224c2a676d1a
= <dominikasmorag@gmail.com>
2024-02-11 13:13:15 +0100

    replaced concatenation with append method

--- src/main/java/pl/dominikasmorag/ui/export/ExportHTML.java
+++ src/main/java/pl/dominikasmorag/ui/export/ExportHTML.java
@@ -12,21 +12,13 @@ public class ExportHTML extends ExportStrategy {
     private final static String OPEN_TABLE = "<html> <body> <table>";
     private final static String CLOSE_TABLE = "</table> </body> </html>";
     private final static String[] COLUMN_NAMES = {"Id", "Location", "Description", "Square meters", "Price [PLN]", "Link", "Image url", "Posting date", "DateTime of scraping"};
-    private final static String CSS = "<style>\n" +
-            "    table {\n" +
-            "        border-collapse: collapse;\n" +
-            "        width: 100%;\n" +
-            "    }\n" +
-            "\n" +
-            "    th, td {\n" +
-            "        border: 1px solid #dddddd;\n" +
-            "        text-align: left;\n" +
-            "        padding: 8px;\n" +
-            "    }\n" +
-            "\n" +
-            "    th {\n" +
-            "        background-color: #f2f2f2;\n" +
-            "    }\n" +
+    private final static String CSS = "<style>" +
+            "table { border-collapse: collapse;" +
+            "       width: 100%; }" +
+            "th, td { border: 1px solid #dddddd;" +
+            "        text-align: left;" +
+            "        padding: 8px; }" +
+            "th { background-color: #f2f2f2; }" +
             "</style>";
 
     public ExportHTML(DAO<Result> resultDao) {
@@ -60,7 +52,7 @@ public class ExportHTML extends ExportStrategy {
         sb.append(CSS);
         sb.append("<tr>");
         for(String s : COLUMN_NAMES) {
-            sb.append("<th>" + s + "</th>");
+            sb.append("<th>").append(s).append("</th>");
         }
         sb.append("</tr>");
 
