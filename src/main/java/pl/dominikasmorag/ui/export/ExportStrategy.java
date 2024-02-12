07e714091fe7e39bde8089b50eee181fdbd0c9ca
= <dominikasmorag@gmail.com>
2024-02-12 08:46:09 +0100

    now COLUMN_NAMES are in this class available for each subclass

--- src/main/java/pl/dominikasmorag/ui/export/ExportStrategy.java
+++ src/main/java/pl/dominikasmorag/ui/export/ExportStrategy.java
@@ -14,21 +14,28 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 public abstract class ExportStrategy {
-     List<Result> resultList;
-     DAO<Result> dao;
+     protected List<Result> resultList;
+
+     private final DAO<Result> dao;
      private final SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyyHHmmss");
      protected String fileName = formatter.format(new Date(System.currentTimeMillis()));
      protected String filePath = "C:\\Users\\domin\\Desktop\\real_estate_archive_reports\\";
+
+     protected final static String[] COLUMN_NAMES = {"Id",
+             "Location",
+             "Description",
+             "Square meters",
+             "Price [PLN]",
+             "Link",
+             "Image url",
+             "Posting date",
+             "DateTime of scraping"};
      public ExportStrategy(DAO<Result> dao) {
           this.dao = dao;
      }
 
      protected File createFile() throws IOException {
-          File f = new File(filePath + fileName);
-          if(!f.exists()) {
-               f.createNewFile();
-          }
-          return f;
+          return new File(filePath + fileName);
      }
 
       protected void collectData() {
@@ -37,15 +44,13 @@ public abstract class ExportStrategy {
           } catch (SQLException ex) {
                ex.printStackTrace();
           }
-      };
+      }
 
      public abstract void export()  throws IOException;
 
-
      protected BufferedWriter createBufferedWriter(File file) {
           try {
-               System.out.println("FILE NAME " + file.getPath());
-               System.out.println("Tried to bw");
+               System.out.println("File path: " + file.getPath());
                return new BufferedWriter(new FileWriter(file));
           } catch (IOException ex) {
                ex.printStackTrace();
