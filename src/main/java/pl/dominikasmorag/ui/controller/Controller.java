b9df14aa12ea82276c6488c41ae10b99b8d1f0d4
= <dominikasmorag@gmail.com>
2024-02-13 09:06:48 +0100

    controller class to get connection with db and use ui

--- src/main/java/pl/dominikasmorag/ui/controller/Controller.java
+++ src/main/java/pl/dominikasmorag/ui/controller/Controller.java
@@ -1,2 +1,51 @@
-package pl.dominikasmorag.ui.controller;public class Controller {
+package pl.dominikasmorag.ui.controller;
+
+import org.h2.jdbcx.JdbcDataSource;
+import pl.dominikasmorag.database.DataBase;
+import pl.dominikasmorag.database.ResultDao;
+import pl.dominikasmorag.ui.command.Command;
+import pl.dominikasmorag.ui.command.CommandFactory;
+import pl.dominikasmorag.website.ResultInfo;
+import pl.dominikasmorag.website.WebsiteInfo;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Controller {
+    static ResultDao resultDao;
+    static ResultInfo resultInfo;
+    private static Connection connection;
+
+    public static void initialize(JdbcDataSource dataSource) {
+        try {
+            connection = dataSource.getConnection();
+            DataBase db = new DataBase(connection);
+            db.createSchema();
+            resultDao = new ResultDao(connection);
+            WebsiteInfo websiteInfo = WebsiteInfo.getInstance();
+            resultInfo = new ResultInfo(websiteInfo);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void allowCommands() {
+        String userInput = "";
+        while (!("exit".equalsIgnoreCase(userInput))) {
+            Scanner sc = new Scanner(System.in);
+            userInput = sc.nextLine();
+            try {
+                Command command = CommandFactory.createCommand(userInput, resultDao);
+                command.execute();
+            } catch (NullPointerException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static ResultDao getResultDao() {
+        return resultDao;
+    }
+
 }
