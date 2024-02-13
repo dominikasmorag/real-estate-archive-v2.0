9163d366f69c963456b6cb88c22257dc113c3341
= <dominikasmorag@gmail.com>
2024-02-13 09:07:20 +0100

    another command

--- src/main/java/pl/dominikasmorag/ui/command/ScrapingCommand.java
+++ src/main/java/pl/dominikasmorag/ui/command/ScrapingCommand.java
@@ -1,2 +1,30 @@
-package pl.dominikasmorag.ui.command;public class ScrapingCommand {
+package pl.dominikasmorag.ui.command;
+
+import pl.dominikasmorag.database.ResultDao;
+import pl.dominikasmorag.pojo.Result;
+import pl.dominikasmorag.website.ResultInfo;
+import pl.dominikasmorag.website.WebsiteInfo;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ScrapingCommand implements Command {
+    private final ResultDao resultDao;
+
+    public ScrapingCommand(ResultDao resultDao) {
+        this.resultDao = resultDao;
+    }
+    @Override
+    public void execute() {
+        System.out.println("Scraping started, please wait...");
+        ResultInfo resultInfo = new ResultInfo(WebsiteInfo.getInstance());
+        try {
+            List<Result> results = resultInfo.scrapAllResults(resultDao.findAllLinks());
+            resultDao.saveAll(results);
+        } catch (IOException |SQLException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Scraping finished");
+    }
 }
