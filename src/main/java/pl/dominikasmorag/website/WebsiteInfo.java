12c76321ebf465c7e8cbef219d383f4cbce471ff
= <dominikasmorag@gmail.com>
2024-02-13 09:09:27 +0100

    removed unnecessary numberOfPage variable

--- src/main/java/pl/dominikasmorag/website/WebsiteInfo.java
+++ src/main/java/pl/dominikasmorag/website/WebsiteInfo.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class WebsiteInfo {
     private static WebsiteInfo instance;
     private static final int FIRST_PAGE = 1;
-    int numberOfPage;
     String URL1 = "https://domy.pl/mieszkania-sprzedaz-katowice-pl?page=";
     String URL2 = "&ps[advanced_search]=1&ps[sort_order]=rank&ps[sort_asc]=0&ps[location][type]=1&ps[location][text_queue][0]=Katowice&ps[location][text_tmp_queue][0]=Katowice&ps[transaction]=1&ps[type]=1&ps[price_from]=200000&ps[price_to]=500000&limit=75";
 
@@ -45,7 +44,6 @@ public class WebsiteInfo {
         try {
             int numberOfPages = pages();
             for (int i = 1; i <= numberOfPages; i++) {
-                numberOfPage = i;
                 list.add(URL1 + i + URL2);
             }
         } catch (IOException ex) {
