d1d3ef7a6fec4ee1b34c0f3c2958f7e237c56c4c
= <dominikasmorag@gmail.com>
2024-02-14 20:17:21 +0100

    command for displaying available commands

--- src/main/java/pl/dominikasmorag/ui/command/HelpCommand.java
+++ src/main/java/pl/dominikasmorag/ui/command/HelpCommand.java
@@ -1,11 +1,13 @@
 package pl.dominikasmorag.ui.command;
 
-public class DisplayCommand implements Command {
+public class HelpCommand implements Command {
+    private final static String TEXT_TO_DISPLAY = """
+            help - displays all available commands
+            generate-report [json, html, csv] - generates a report in desired format
+            initiate-webscraping - starts webscraping
+            exit - stops program""";
     @Override
     public void execute() {
-        System.out.println("help - displays all available commands\n" +
-                "generate-report [json, html, csv] - generates a report in desired format\n" +
-                "initiate-webscraping - starts webscraping\n" +
-                "exit - stops program");
+        System.out.println(TEXT_TO_DISPLAY);
     }
 }
